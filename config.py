4d69262f2d74c39e871f42b36e84d5bbcc5d8924
Stan <stan.bruyere01@gmail.com>
2020-10-28 23:43:45 +0100

    ready for prod

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ SECRET_KEY = "$e282.6$z7L+Hi@'x`cQ(q+w"
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
